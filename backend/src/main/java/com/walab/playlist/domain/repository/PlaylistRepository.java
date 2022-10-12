61d83b00ac1da61012693f94e2b3efa9c7b90396
sklee <leevinodi@gmail.com>
2022-10-12 19:27:08 +0900

    [#142] api: 마이플레이리스트 검색 api 구현

--- backend/src/main/java/com/walab/playlist/domain/repository/PlaylistRepository.java
+++ backend/src/main/java/com/walab/playlist/domain/repository/PlaylistRepository.java
@@ -2,14 +2,15 @@ package com.walab.playlist.domain.repository;
 
 import com.walab.playlist.domain.Playlist;
 
+import com.walab.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
-
     @Query("select p from Playlist p " +
             "left join fetch p.videos as v " +
             "where p.id = :id")
@@ -18,4 +19,6 @@ public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
     @Query("select p from Playlist p " +
             "where p.user.id = :userId ")
     List<Playlist> getPlaylistNameByUserId(Long userId);
+
+    List<Playlist> findPlaylistByUserIdAndPlaylistNameContainsIgnoreCase(@Param("userId") Long userId, @Param("playlistName") String playlistName);
 }
