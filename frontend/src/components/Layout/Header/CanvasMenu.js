566a413001b20611c6138c5e031af30cae18bda4
JuEun <86585240+kimjueun1@users.noreply.github.com>
2022-10-26 09:09:59 +0900

    작업중 (#156)

--- frontend/src/components/Layout/Header/CanvasMenu.js
+++ frontend/src/components/Layout/Header/CanvasMenu.js
@@ -2,6 +2,7 @@ import React, { useState, useCallback } from "react";
 import { Link } from "react-router-dom";
 import Gallery from "react-photo-gallery";
 import Carousel, { Modal, ModalGateway } from "react-images";
+import { useHistory } from "react-router-dom";
 
 import galleryLogo1 from "../../../assets/img/gallery/1.jpg";
 import galleryLogo2 from "../../../assets/img/gallery/2.jpg";
@@ -10,126 +11,181 @@ import galleryLogo4 from "../../../assets/img/gallery/4.jpg";
 import galleryLogo5 from "../../../assets/img/gallery/5.jpg";
 import galleryLogo6 from "../../../assets/img/gallery/6.jpg";
 import mapImg from "../../../assets/img/map.jpg";
-
+import Login from "../../../pages/login";
+import CourseSection from "../../../pages/dashboard/CourseSection";
+import CourseMainAdmin from "../../../pages/dashboard/CourseMainAdmin";
+import CourseMainClosed from "../../../pages/dashboard/CourseMainClosed";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import { AiOutlinePlayCircle } from "react-icons/ai";
 const photos = [
-    {
-        src: galleryLogo1,
-        width: 1,
-        height: 1,
-    },
-    {
-        src: galleryLogo2,
-        width: 1,
-        height: 1,
-    },
-    {
-        src: galleryLogo3,
-        width: 1,
-        height: 1,
-    },
-    {
-        src: galleryLogo4,
-        width: 1,
-        height: 1,
-    },
-    {
-        src: galleryLogo5,
-        width: 1,
-        height: 1,
-    },
-    {
-        src: galleryLogo6,
-        width: 1,
-        height: 1,
-    },
+  {
+    src: galleryLogo1,
+    width: 1,
+    height: 1,
+  },
+  {
+    src: galleryLogo2,
+    width: 1,
+    height: 1,
+  },
+  {
+    src: galleryLogo3,
+    width: 1,
+    height: 1,
+  },
+  {
+    src: galleryLogo4,
+    width: 1,
+    height: 1,
+  },
+  {
+    src: galleryLogo5,
+    width: 1,
+    height: 1,
+  },
+  {
+    src: galleryLogo6,
+    width: 1,
+    height: 1,
+  },
 ];
-
+const userId = window.sessionStorage.getItem("userId");
+let tab1 = "수강중인 강의실",
+  tab2 = "관리중인 강의실",
+  tab3 = "종료된 강의실",
+  tabStyle = "intro-tabs tabs-box";
 const CanvasMenu = (props) => {
-    const { canvasClass, canvasLogo } = props;
+  const { canvasClass, canvasLogo } = props;
 
-    const canvasMenuRemove = () => {
-        document.body.classList.remove("nav-expanded");
-    };
+  const canvasMenuRemove = () => {
+    document.body.classList.remove("nav-expanded");
+  };
 
-    const [currentImage, setCurrentImage] = useState(0);
-    const [viewerIsOpen, setViewerIsOpen] = useState(false);
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
-    const openLightbox = useCallback((event, { photo, index }) => {
-        setCurrentImage(index);
-        setViewerIsOpen(true);
-    }, []);
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
 
-    const closeLightbox = () => {
-        setCurrentImage(0);
-        setViewerIsOpen(false);
-    };
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+  let id = window.sessionStorage.getItem("userId");
+  const history = useHistory();
 
-    return (
-        <React.Fragment>
-            <nav className={canvasClass}>
-                <div className="close-btn">
-                    <div onClick={canvasMenuRemove} id="nav-close">
-                        <div className="line">
-                            <span className="line1"></span>
-                            <span className="line2"></span>
-                        </div>
-                    </div>
-                </div>
-                <div className="canvas-logo">
-                    <Link to="/learntube/">
-                        <img src={canvasLogo} alt="logo" />
-                    </Link>
-                </div>
-                <div className="offcanvas-text">
-                    <p>We denounce with righteous indige nationality and dislike men who are so beguiled and demo by the charms of pleasure of the moment data com so blinded by desire.</p>
-                </div>
-                <div className="offcanvas-gallery">
-                    <Gallery photos={photos} onClick={openLightbox} />
-                    <ModalGateway>
-                        {viewerIsOpen ? (
-                            <Modal onClose={closeLightbox}>
-                                <Carousel
-                                    currentIndex={currentImage}
-                                    views={photos.map((x) => ({
-                                        ...x,
-                                        srcset: x.srcSet,
-                                        caption: x.title,
-                                    }))}
-                                />
-                            </Modal>
-                        ) : null}
-                    </ModalGateway>
-                </div>
-                <div className="map-img">
-                    <img src={mapImg} alt="" />
-                </div>
-                <div className="canvas-contact">
-                    <ul className="social">
-                        <li>
-                            <a href="#">
-                                <i className="fa fa-facebook-f"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i className="fa fa-twitter"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i className="fa fa-pinterest"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i className="fa fa-instagram"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </React.Fragment>
-    );
+  const clickDashBoard = () => {
+    if (id) {
+      history.replace({
+        pathname: "/learntube/dashboard",
+      });
+    } else {
+      alert("로그인이 필요합니다.");
+    }
+  };
+  return (
+    <React.Fragment>
+      <nav className={canvasClass} onMouseLeave={canvasMenuRemove}>
+        <Login />
+        <ul
+          style={{
+            backgroundColor: "#f8f9fa",
+            textAlign: "left",
+            color: "black",
+          }}
+        >
+          <li>
+            <Link onClick={clickDashBoard}>
+              <AiOutlinePlayCircle />
+              {tab1}
+              {/* <CourseSection userId={userId} /> */}
+            </Link>
+          </li>
+          <li>
+            <Link onClick={clickDashBoard}>{tab2}</Link>
+          </li>
+          <li>
+            <Link onClick={clickDashBoard}>{tab3}</Link>
+          </li>
+        </ul>
+        {/* <TabPanel>
+            <CourseMain userId={userId} />
+          </TabPanel>
+          <TabPanel>
+            <CourseMainAdmin userId={userId} />
+          </TabPanel>
+          <TabPanel>
+            <CourseMainClosed userId={userId} />
+          </TabPanel> */}
+        {/* <div className="close-btn">
+          <div onClick={canvasMenuRemove} id="nav-close">
+            <div className="line">
+              <span className="line1"></span>
+              <span className="line2"></span>
+            </div>
+          </div>
+        </div> */}
+        {/* <div className="canvas-logo">
+          <Link to="/learntube/">
+            <img src={canvasLogo} alt="logo" />
+          </Link>
+        </div>
+        <div className="offcanvas-text">
+          <p>
+            We denounce with righteous indige nationality and dislike men who
+            are so beguiled and demo by the charms of pleasure of the moment
+            data com so blinded by desire.
+          </p>
+        </div>
+        <div className="offcanvas-gallery">
+          <Gallery photos={photos} onClick={openLightbox} />
+          <ModalGateway>
+            {viewerIsOpen ? (
+              <Modal onClose={closeLightbox}>
+                <Carousel
+                  currentIndex={currentImage}
+                  views={photos.map((x) => ({
+                    ...x,
+                    srcset: x.srcSet,
+                    caption: x.title,
+                  }))}
+                />
+              </Modal>
+            ) : null}
+          </ModalGateway>
+        </div>
+        <div className="map-img">
+          <img src={mapImg} alt="" />
+        </div>
+        <div className="canvas-contact">
+          <ul className="social">
+            <li>
+              <a href="#">
+                <i className="fa fa-facebook-f"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa fa-twitter"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa fa-pinterest"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa fa-instagram"></i>
+              </a>
+            </li>
+          </ul>
+        </div> */}
+      </nav>
+    </React.Fragment>
+  );
 };
 
 export default CanvasMenu;
--- frontend/src/components/Widget/ContentButton/ContentButton.js
+++ frontend/src/components/Widget/ContentButton/ContentButton.js
@@ -1,143 +1,162 @@
 import React, { useEffect, useState } from "react";
 
-const ContentButton = ({ lectureNum, setLectureNum, contentId, setContentId, contentNum, setContentNum, playing, setPlaying, lectures, videoNum, setVideoNum }) => {
-    const prevContentHandler = () => {
-        if (contentNum < 0) {
-            setContentNum(0);
-            setContentId(lectures[lectureNum - 1].contents[contentNum - 1].contentId);
-            setPlaying(false);
-        }
-        if (contentNum > 0) setContentNum(contentNum - 1);
-        setContentId(lectures[lectureNum - 1].contents[contentNum - 1].contentId);
-        setPlaying(false);
-        setVideoNum(0);
-    };
-    const nextContentHandler = () => {
-        if (contentNum < 0) setContentNum(0);
-        setContentNum(contentNum + 1);
-        setContentId(lectures[lectureNum - 1].contents[contentNum + 1].contentId);
-        setPlaying(false);
-        setVideoNum(0);
-    };
-    useEffect(() => {}, [contentId, lectureNum]);
-    return (
+const ContentButton = ({
+  lectureNum,
+  setLectureNum,
+  contentId,
+  setContentId,
+  contentNum,
+  setContentNum,
+  playing,
+  setPlaying,
+  lectures,
+  videoNum,
+  setVideoNum,
+}) => {
+  const prevContentHandler = () => {
+    if (contentNum < 0) {
+      setContentNum(0);
+      setContentId(lectures[lectureNum - 1].contents[contentNum - 1].contentId);
+      setPlaying(false);
+    }
+    if (contentNum > 0) setContentNum(contentNum - 1);
+    setContentId(lectures[lectureNum - 1].contents[contentNum - 1].contentId);
+    setPlaying(false);
+    setVideoNum(0);
+  };
+  const nextContentHandler = () => {
+    if (contentNum < 0) setContentNum(0);
+    setContentNum(contentNum + 1);
+    setContentId(lectures[lectureNum - 1].contents[contentNum + 1].contentId);
+    setPlaying(false);
+    setVideoNum(0);
+  };
+  useEffect(() => {}, [contentId, lectureNum]);
+  return (
+    <>
+      {" "}
+      {/* 이전 콘텐츠 버튼 */}
+      {lectures[lectureNum - 1].contents.length != 1 ? (
         <>
-            {" "}
-            {/* 이전 콘텐츠 버튼 */}
-            {lectures[lectureNum - 1].contents.length != 1 ? (
-                <>
-                    {/* {!props.lectures[lectureNum - 1].contents[0] ? ( */}
-                    <ul
-                        className={contentNum != 0 ? "pagination-part shadow-none border-0 contentBtn" : "pagination-part shadow-none border-0 contentBtn_disabled"}
-                        style={{ cursor: "pointer", backgroundColor: "#a5abbd" }}
-                    >
-                        {contentNum != 0 ? (
-                            <li onClick={prevContentHandler} style={{ fontSize: "0.9rem" }}>
-                                <i className="fa fa-play" style={{ transform: "rotate(180deg)" }}></i>
-                                &ensp;이전 콘텐츠
-                            </li>
-                        ) : (
-                            <li style={{ fontSize: "0.9rem" }}>
-                                <i
-                                    className="fa fa-play"
-                                    style={{
-                                        transform: "rotate(180deg)",
-                                        // color: "gray",
-                                    }}
-                                ></i>
-                                &ensp;이전 콘텐츠
-                            </li>
-                        )}
-                    </ul>
-                </>
+          {/* {!props.lectures[lectureNum - 1].contents[0] ? ( */}
+          <ul
+            className={
+              contentNum != 0
+                ? "pagination-part shadow-none border-0 contentBtn"
+                : "pagination-part shadow-none border-0 contentBtn_disabled"
+            }
+            style={{ cursor: "pointer", backgroundColor: "#a5abbd" }}
+          >
+            {contentNum != 0 ? (
+              <li onClick={prevContentHandler} style={{ fontSize: "0.9rem" }}>
+                <i
+                  className="fa fa-play"
+                  style={{ transform: "rotate(180deg)" }}
+                ></i>
+                &ensp;이전 콘텐츠
+              </li>
             ) : (
-                <>
-                    {/* {!props.lectures[lectureNum - 1].contents[0] ? ( */}
-                    {contentNum != 0 ? (
-                        <ul
-                            className="pagination-part shadow-none border-0 contentBtn"
-                            // style={{ cursor: "pointer", backgroundColor: "#a5abbd" }}
-                        >
-                            <li style={{ fontSize: "0.9rem" }}>
-                                <i
-                                    className="fa fa-play"
-                                    style={{
-                                        transform: "rotate(180deg)",
-                                        // color: "#fff",
-                                    }}
-                                ></i>
-                                &ensp;이전 콘텐츠
-                            </li>
-                        </ul>
-                    ) : (
-                        <ul
-                            className="pagination-part shadow-none border-0 contentBtn_disabled"
-                            // style={{ cursor: "pointer", backgroundColor: "#a5abbd" }}
-                        >
-                            <li style={{ fontSize: "0.9rem" }}>
-                                <i
-                                    className="fa fa-play"
-                                    style={{
-                                        transform: "rotate(180deg)",
-                                        // color: "gray",
-                                    }}
-                                ></i>
-                                &ensp;이전 콘텐츠
-                            </li>
-                        </ul>
-                    )}
-                </>
+              <li style={{ fontSize: "0.9rem" }}>
+                <i
+                  className="fa fa-play"
+                  style={{
+                    transform: "rotate(180deg)",
+                    // color: "gray",
+                  }}
+                ></i>
+                &ensp;이전 콘텐츠
+              </li>
             )}
-            {lectures[lectureNum - 1].contents.length != 1 ? (
-                <>
-                    {/* <h>{props.lectures[lectureNum - 1].contents.length}</h>
-{contentNum} */}
-                    {lectures[lectureNum - 1].contents.length != contentNum + 1 ? (
-                        <ul
-                            className="pagination-part shadow-none border-0 contentBtn"
-                            // style={{ cursor: "pointer", backgroundColor: "#a5abbd" }}
-                        >
-                            <li onClick={nextContentHandler} style={{ fontSize: "0.9rem" }}>
-                                다음 콘텐츠&ensp;<i className="fa fa-play"></i>
-                            </li>
-                        </ul>
-                    ) : (
-                        <ul
-                            className="pagination-part shadow-none border-0 contentBtn_disabled"
-                            // style={{ cursor: "pointer", backgroundColor: "#a5abbd" }}
-                        >
-                            <li style={{ fontSize: "0.9rem" }}>
-                                다음 콘텐츠&ensp;
-                                <i className="fa fa-play"></i>
-                            </li>
-                        </ul>
-                    )}
-                </>
-            ) : (
-                <>
-                    {lectures[lectureNum - 1].contents.length != contentNum + 1 ? (
-                        <ul
-                            className="pagination-part shadow-none border-0 contentBtn"
-                            // style={{ cursor: "pointer", backgroundColor: "#a5abbd" }}
-                        >
-                            <li style={{ fontSize: "0.9rem" }}>
-                                다음 콘텐츠&ensp;<i className="fa fa-play"></i>
-                            </li>
-                        </ul>
-                    ) : (
-                        <ul
-                            className="pagination-part shadow-none border-0 contentBtn_disabled"
-                            // style={{ cursor: "pointer", backgroundColor: "#a5abbd" }}
-                        >
-                            <li style={{ fontSize: "0.9rem" }}>
-                                다음 콘텐츠&ensp;
-                                <i className="fa fa-play"></i>
-                            </li>
-                        </ul>
-                    )}
-                </>
-            )}
+          </ul>
         </>
-    );
+      ) : (
+        <>
+          {/* {!props.lectures[lectureNum - 1].contents[0] ? ( */}
+          {contentNum != 0 ? (
+            <ul
+              className="pagination-part shadow-none border-0 contentBtn"
+              // style={{ cursor: "pointer", backgroundColor: "#a5abbd" }}
+            >
+              <li style={{ fontSize: "0.9rem" }}>
+                <i
+                  className="fa fa-play"
+                  style={{
+                    transform: "rotate(180deg)",
+                    // color: "#fff",
+                  }}
+                ></i>
+                &ensp;이전 콘텐츠
+              </li>
+            </ul>
+          ) : (
+            <ul
+              className="pagination-part shadow-none border-0 contentBtn_disabled"
+              // style={{ cursor: "pointer", backgroundColor: "#a5abbd" }}
+            >
+              <li style={{ fontSize: "0.9rem" }}>
+                <i
+                  className="fa fa-play"
+                  style={{
+                    transform: "rotate(180deg)",
+                    // color: "gray",
+                  }}
+                ></i>
+                &ensp;이전 콘텐츠
+              </li>
+            </ul>
+          )}
+        </>
+      )}
+      {lectures[lectureNum - 1].contents.length != 1 ? (
+        <>
+          {/* <h>{props.lectures[lectureNum - 1].contents.length}</h>
+{contentNum} */}
+          {lectures[lectureNum - 1].contents.length != contentNum + 1 ? (
+            <ul
+              className="pagination-part shadow-none border-0 contentBtn"
+              // style={{ cursor: "pointer", backgroundColor: "#a5abbd" }}
+            >
+              <li onClick={nextContentHandler} style={{ fontSize: "0.9rem" }}>
+                다음 콘텐츠&ensp;<i className="fa fa-play"></i>
+              </li>
+            </ul>
+          ) : (
+            <ul
+              className="pagination-part shadow-none border-0 contentBtn_disabled"
+              // style={{ cursor: "pointer", backgroundColor: "#a5abbd" }}
+            >
+              <li style={{ fontSize: "0.9rem" }}>
+                다음 콘텐츠&ensp;
+                <i className="fa fa-play"></i>
+              </li>
+            </ul>
+          )}
+        </>
+      ) : (
+        <>
+          {lectures[lectureNum - 1].contents.length != contentNum + 1 ? (
+            <ul
+              className="pagination-part shadow-none border-0 contentBtn"
+              // style={{ cursor: "pointer", backgroundColor: "#a5abbd" }}
+            >
+              <li style={{ fontSize: "0.9rem" }}>
+                다음 콘텐츠&ensp;<i className="fa fa-play"></i>
+              </li>
+            </ul>
+          ) : (
+            <ul
+              className="pagination-part shadow-none border-0 contentBtn_disabled"
+              // style={{ cursor: "pointer", backgroundColor: "#a5abbd" }}
+            >
+              <li style={{ fontSize: "0.9rem" }}>
+                다음 콘텐츠&ensp;
+                <i className="fa fa-play"></i>
+              </li>
+            </ul>
+          )}
+        </>
+      )}
+    </>
+  );
 };
 export default ContentButton;
