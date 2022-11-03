5d5e8ca63a6e349aef313c3f476622576706ed1d
JuEun <86585240+kimjueun1@users.noreply.github.com>
2022-11-03 11:57:28 +0900

    Fe/feat/#167 (#174)
    * [#167] fix: dashboard -> dashboard-main url 변경
    
    * [#167] feat: 플레이리스트 디자인, 순서변경 구현중

--- frontend/src/components/Widget/PlaylistSearchWidget.js
+++ frontend/src/components/Widget/PlaylistSearchWidget.js
@@ -1,21 +1,42 @@
 import React, { useState } from "react";
 
 const PlaylistSearchWidget = ({ findSearchData }) => {
-    const [search, setSearch] = useState("");
-    const onChangeSearch = (e) => {
-        setSearch(e.target.value);
-    };
+  const [search, setSearch] = useState("");
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
 
-    return (
-        <div className="search-widget" style={{ background: "none", boxShadow: "none" }}>
-            <div className="search-wrap" style={{ width: "280px", height: "55px", left: "110px" }}>
-                <input type="search" placeholder="플레이리스트 검색..." name="s" onChange={onChangeSearch} style={{ padding: "4px" }} />
-                <button className="p-2" type="submit" onClick={() => findSearchData(search)} style={{ fontSize: "10px" }}>
-                    <i className="fa fa-search" style={{ background: "white", borderRadius: "20px" }}></i>
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className="search-widget"
+      style={{ background: "none", boxShadow: "none" }}
+    >
+      <div
+        className="search-wrap"
+        style={{ width: "280px" }}
+        // style={{ width: "280px", height: "55px", left: "110px" }}
+      >
+        <input
+          type="search"
+          placeholder="플레이리스트 검색..."
+          name="s"
+          onChange={onChangeSearch}
+          style={{ padding: "4px" }}
+        />
+        <button
+          className="p-2"
+          type="submit"
+          onClick={() => findSearchData(search)}
+          style={{ fontSize: "10px" }}
+        >
+          <i
+            className="fa fa-search"
+            style={{ background: "white", borderRadius: "20px" }}
+          ></i>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default PlaylistSearchWidget;
