66be7269c331e5fba21016a9d50dfe9fdc327bc5
sklee <leevinodi@gmail.com>
2022-11-29 16:21:17 +0900

    [#227] feat: 에러 fix

--- backend/src/main/java/com/walab/exception/ExceptionApiController.java
+++ backend/src/main/java/com/walab/exception/ExceptionApiController.java
@@ -1,2 +1,31 @@
-package com.walab.exception;public class ExceptionApiController {
+package com.walab.exception;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.Objects;
+
+@Controller
+public class ExceptionApiController implements ErrorController {
+
+    @GetMapping({"/", "error"})
+    public String index(){
+        return "index.html";
+    }
+
+    @RequestMapping(value = "/error")
+    public String handleNoHandleFoundException(HttpServletResponse response, HttpServletRequest request) {
+        return "/error";
+    }
 }
