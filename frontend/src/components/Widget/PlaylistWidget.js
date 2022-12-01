2fe188d30b4879fdd920685e5bbe108cfc9ff7fd
JuEun <moomin@handong.ac.kr>
2022-12-01 14:22:11 +0900

    [#254] fix: 플리 삭제 플리명 입력 다시머지

--- frontend/src/components/Widget/PlaylistWidget.js
+++ frontend/src/components/Widget/PlaylistWidget.js
@@ -65,6 +65,9 @@ const PlaylistWidget = ({
   const [endTime, setEndTime] = useState(null);
   const [cardView, setCardView] = useState(false);
   const [videoIdx, setvideoIdx] = useState();
+  const [isOnDelete, setIsOnDelete] = useState(false);
+  const openDeleteModal = () => setIsOnDelete(!isOnDelete);
+  const [deletePlaylistName, setDeletePlaylistName] = useState("");
   //   console.log(selectedVideo[videoNum]);
   //   console.log(managedClassroom);
   //   let classroomData = [];
@@ -372,6 +375,27 @@ const PlaylistWidget = ({
     setUpdatePlaylistTitle("");
     setSavedPlaylistName(selectedPlaylist);
   };
+  const handleDeleteChange = (e) => {
+    if (e.target.value !== null) {
+      setDeletePlaylistName(e.target.value);
+    }
+  };
+  const onDelete = () => {
+    if (deletePlaylistName && deletePlaylistName === savedPlaylistName) {
+      setIsOnDelete(false);
+      if (window.confirm("플레이리스트를 삭제하시겠습니까?")) {
+        deletePlaylist();
+      } else {
+        return;
+      }
+    } else {
+      setDeletePlaylistName("");
+      window.alert("플레이리스트 제목이 일치하지 않습니다.");
+      openDeleteModal();
+      return;
+    }
+  };
+
   return (
     <div
     // className="row justify-content-center"
@@ -1383,7 +1407,8 @@ const PlaylistWidget = ({
           <div className="col mt-40 d-flex justify-content-center align-items-center">
             <Button
               className="btn btn-sm"
-              onClick={() => deletePlaylist()}
+              // onClick={() => deletePlaylist()}
+              onClick={() => openDeleteModal()}
               style={{ backgroundColor: "red" }}
             >
               플레이리스트 삭제
@@ -1391,6 +1416,84 @@ const PlaylistWidget = ({
           </div>
         )}
       </div>
+      {isOnDelete ? (
+        <>
+          <div
+            style={{
+              zIndex: "100",
+              position: "fixed",
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              backgroundColor: "rgb(0, 0, 0, 0.20)",
+            }}
+          >
+            <div
+              style={{
+                position: "absolute",
+                top: "15%",
+                left: "25%",
+                right: "25%",
+                background: "#fff",
+                overflow: "auto",
+                WebkitOverflowScrolling: "touch",
+                outline: "none",
+                padding: "0px",
+              }}
+            >
+              <div className="register-section w-100 h-100">
+                <div className="container">
+                  <div className="py-3 px-5">
+                    <div className="styled-form">
+                      <div id="form-messages"></div>
+
+                      <form id="contact-form">
+                        <div className="row clearfix">
+                          <div className="form-group col-lg-12 mb-25">
+                            <h5 className="my-2">
+                              '{savedPlaylistName}' 플레이리스트를
+                              삭제하시겠습니까?
+                            </h5>
+                            <input
+                              type="text"
+                              id="className"
+                              name="className"
+                              placeholder="삭제하려는 현재 플레이리스트 이름을 입력해 주세요."
+                              onChange={handleDeleteChange}
+                            />
+                          </div>
+                        </div>
+                        <div className="row d-flex justify-content-end ms-3 me-1 mt-10">
+                          <Button
+                            className="col-2 btn-secondary btn-sm"
+                            onClick={() => {
+                              openDeleteModal();
+                            }}
+                          >
+                            취소
+                          </Button>
+                          <Button
+                            className="ml-10 col-2 btn-sm"
+                            style={{ background: "red" }}
+                            onClick={() => {
+                              onDelete();
+                            }}
+                          >
+                            삭제
+                          </Button>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
